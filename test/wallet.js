fd7665a35c4f8a7c14df7b338835e8ec5c5b4552
jackoiu8722 <jackliu8722@gmail.com>
2018-07-27 19:58:54 +0800

    Import from private key

--- test/wallet.js
+++ test/wallet.js
@@ -3,17 +3,20 @@ var Wallet = require("../lib/wallet")
 
 var words = "club hedgehog guide door stock submit catch time lobster negative visit praise"
 var password = "12345678"
+var privateKey = "0x66e375a8b425af65db846eb065a96a84b30a551f117581e6a71bb22a346c6140"
 
 describe("Wallet with Mnemonic",function(){
 
-    it("generate and import",function(){
+    it("generate and import",function(done){
 
         console.log(words)
         var w = new Wallet()
         w.importFromMnemonic(words,password)
         var ww = w._wallet;
+        console.log(ww.getPrivateKey())
         var ks = ww.toV3("12345678")
         console.log(ks)
+        done();
     }),
 
     it("Is valid",function(){
@@ -27,4 +30,21 @@ describe("Wallet with Mnemonic",function(){
         console.log(wordslist)
     })
 
+})
+
+describe("Wallet with privateKey",function(){
+    it("Generate privateKey",function(done){
+        var w = new Wallet()
+        var wordslist = w.generateFromMnemonic(password)
+        console.log(w.getPrivateKeyString())
+        done()
+    })
+
+    it("Import from private key",function(done){
+        var w = new Wallet()
+        w.importFromPrivateKey(privateKey,password)
+        var privKey = w.getPrivateKeyString()
+        expect(privKey).to.equal(privateKey)
+        done()
+    })
 })
