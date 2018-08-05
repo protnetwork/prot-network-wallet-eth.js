65c8ded4e94284cf14e2e646d647322511cec759
jackoiu8722 <jackliu8722@gmail.com>
2018-08-05 16:54:06 +0800

    Import wallet from keystore

--- test/wallet.js
+++ test/wallet.js
@@ -1,10 +1,12 @@
 var expect = require('chai').expect
 var Wallet = require("../lib/wallet")
+var protUtil = require("../lib/protutil")
+
 
 var words = "club hedgehog guide door stock submit catch time lobster negative visit praise"
 var password = "12345678"
 var privateKey = "0x66e375a8b425af65db846eb065a96a84b30a551f117581e6a71bb22a346c6140"
-
+var ks_ = "";
 describe("Wallet with Mnemonic",function(){
 
     it("generate and import",function(done){
@@ -15,7 +17,8 @@ describe("Wallet with Mnemonic",function(){
         var ww = w._wallet;
         console.log(ww.getPrivateKey())
         var ks = ww.toV3("12345678")
-        console.log(ks)
+        ks_= JSON.stringify(protUtil.objSort(ks))
+        console.log(ks_)
         done();
     }),
 
@@ -45,6 +48,20 @@ describe("Wallet with privateKey",function(){
         w.importFromPrivateKey(privateKey,password)
         var privKey = w.getPrivateKeyString()
         expect(privKey).to.equal(privateKey)
+        done()
+    })
+})
+
+describe("Wallet with keystore",function(){
+    it("Import from keystore",function(done){
+        this.timeout(15000)
+        var w = new Wallet()
+        w.importFromKeystore(ks_,password)
+        var ww = w._wallet;
+        console.log(ww.getPrivateKey())
+        var ks = ww.toV3("12345678")
+        ks = JSON.stringify(protUtil.objSort(ks))
+        console.log(ks)
         done()
     })
 })
